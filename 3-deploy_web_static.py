9508b9cedf8a7c5eef1c1a7c903c2195b02f959f
Akerele Quadri <akereleayomide106@gmail.com>
2025-02-16 08:06:04 -0800

    Ark

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,6 @@ from fabric.api import run
 
 env.hosts =  ['34.229.254.6', '18.204.20.117']
 
-
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
